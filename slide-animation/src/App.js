833ce1417ba2bf227a1ab35fd6542aa7bab74b94
Shreyansh Kourav <74540509+cur10sDEV@users.noreply.github.com>
2022-03-29 22:07:31 +0530

    second-project complete

---
+++ slide-animation/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import { motion } from "framer-motion";
+import { useState, useEffect, useRef } from "react";
+import images from "./images";
+
+function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  return (
+    <div className="App">
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
+          {images.map((image, i) => {
+            return (
+              <motion.div className="item">
+                <img key={i} src={image} alt="" />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
